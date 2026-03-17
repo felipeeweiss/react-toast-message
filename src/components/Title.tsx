e3c3726621301943070b5b760c902ca65038479b
Felipe <felipeew19@gmail.com>
2026-03-17 14:22:24 +0000

    feat: created a Title component

---
+++ src/components/Title.tsx
@@ -0,0 +1,22 @@
+import type { ToastType } from '../types/toast';
+
+type TitleProps = {
+  type: ToastType;
+};
+
+export function Title({ type }: TitleProps) {
+  const textByType = () => {
+    switch (type) {
+      case 'success':
+        return 'Success';
+      case 'info':
+        return 'Information';
+      case 'warning':
+        return 'Warning';
+      default:
+        return 'Error Occurred';
+    }
+  };
+
+  return <h1 className="toast-title">{textByType()}</h1>;
+}
