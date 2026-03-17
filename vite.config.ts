ae182fab7d086a2f4669afc9bde45c67f86bc38c
Felipe <felipeew19@gmail.com>
2026-03-17 14:20:40 +0000

    chore: reconfigured the repo to import styles automatically

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,27 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import dts from 'vite-plugin-dts';
+import { libInjectCss } from 'vite-plugin-lib-inject-css';
 import { resolve } from 'path';
 
 export default defineConfig({
-  plugins: [react(), dts()],
+  plugins: [
+    react(),
+    dts({
+      insertTypesEntry: true,
+    }),
+    libInjectCss(),
+  ],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
       name: 'ReactToastMessage',
-      fileName: 'react-toast-message',
+      formats: ['es', 'cjs'],
+      fileName: (format) => `react-toast-message.${format}.js`,
     },
     rollupOptions: {
       external: ['react', 'react-dom'],
-      output: {
-        globals: {
-          react: 'React',
-        },
-      },
     },
-    cssCodeSplit: false,
+    cssCodeSplit: true,
   },
 });
